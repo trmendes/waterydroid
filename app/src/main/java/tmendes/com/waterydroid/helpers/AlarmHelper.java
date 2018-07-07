6fb52a5518ab87f7808773274449c1a9724afe22
Thiago Mendes <thiago@posteo.de>
2018-07-07 16:36:13 -0300

    Initial commit

---
+++ app/src/main/java/tmendes/com/waterydroid/helpers/AlarmHelper.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2015-2016 The Food Restriction Project Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package tmendes.com.waterydroid.helpers;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import tmendes.com.waterydroid.receivers.BootReceiver;
+import tmendes.com.waterydroid.receivers.NotifierReceiver;
+
+import static android.app.AlarmManager.INTERVAL_DAY;
+
+public class AlarmHelper {
+
+    private AlarmManager alarmManager;
+
+    private final static String ACTION_BD_NOTIFICATION = "tmendes.com.waterydroid.NOTIFICATION";
+
+    public void setAlarm(Context context, long toGoesOffAt) {
+        Calendar defaultToRingAt = Calendar.getInstance();
+
+
+        Calendar now = Calendar.getInstance();
+        defaultToRingAt.setTimeInMillis(toGoesOffAt);
+        defaultToRingAt.set(Calendar.DAY_OF_MONTH, now.get(Calendar.DAY_OF_MONTH));
+
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String formattedDate = df.format(defaultToRingAt.getTime());
+        Log.i("Alarm", "Setting alarm at " + formattedDate);
+
+        toGoesOffAt = defaultToRingAt.getTimeInMillis();
+
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Intent alarmIntent = new Intent(context, NotifierReceiver.class);
+        alarmIntent.setAction(ACTION_BD_NOTIFICATION);
+
+        PendingIntent pendingAlarmIntent = PendingIntent.getBroadcast(context,
+                0,
+                alarmIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                toGoesOffAt,
+                INTERVAL_DAY,
+                pendingAlarmIntent);
+
+        /* Restart if rebooted */
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        context.getPackageManager().setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    public void cancelAlarm(Context context) {
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Intent alarmIntent = new Intent(context, NotifierReceiver.class);
+        alarmIntent.setAction(ACTION_BD_NOTIFICATION);
+
+        PendingIntent pendingAlarmIntent = PendingIntent.getBroadcast(context,
+                0,
+                alarmIntent,
+                0);
+        alarmManager.cancel(pendingAlarmIntent);
+
+        /* Alarm won't start again if device is rebooted */
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+}
---
+++ app/src/main/java/tmendes/com/waterydroid/helpers/NotificationHelper.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2015-2016 The Food Restriction Project Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package tmendes.com.waterydroid.helpers;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
+
+public class NotificationHelper extends ContextWrapper {
+    private NotificationManager notifManager;
+
+    private static final String CHANNEL_ONE_ID = "tmendes.com.waterydroid.CHONE";
+    private static final String CHANNEL_ONE_NAME = "Channel One";
+
+    public NotificationHelper(Context ctx) {
+        super(ctx);
+        createChannels();
+    }
+
+    /* TODO: SNOOZE && COMPLETE ACTION */
+
+    private void createChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ONE_ID,
+                    CHANNEL_ONE_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            getManager().createNotificationChannel(notificationChannel);
+        }
+    }
+
+    public Notification.Builder getNotification(String title,
+                                                String body,
+                                                Bitmap notifyPicture,
+                                                PendingIntent pI) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return new Notification.Builder(getApplicationContext(), CHANNEL_ONE_ID)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setColorized(true)
+                    .setContentIntent(pI)
+                    .setLargeIcon(notifyPicture)
+                    //.setSmallIcon(R.drawable.ic_cake_white_24dp)
+                    .setAutoCancel(true);
+
+        } else {
+            //noinspection deprecation
+            return new Notification.Builder(getApplicationContext())
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setContentIntent(pI)
+                    .setLargeIcon(notifyPicture)
+                    //.setSmallIcon(R.drawable.ic_cake_white_24dp)
+                    .setAutoCancel(true);
+        }
+    }
+
+    public void notify(long id, Notification.Builder notification) {
+        getManager().notify((int) id, notification.build());
+    }
+
+    private NotificationManager getManager() {
+        if (notifManager == null) {
+            notifManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return notifManager;
+    }
+}
