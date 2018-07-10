c791a9bf07a5e658bf332813541d7ae7d98e926a
Thiago Mendes <thiago@posteo.de>
2018-07-10 11:11:09 -0300

    headers

--- app/src/main/java/tmendes/com/waterydroid/helpers/NotificationHelper.java
+++ app/src/main/java/tmendes/com/waterydroid/helpers/NotificationHelper.java
@@ -1,5 +1,4 @@
 /*
- * Copyright (C) 2015-2016 The Food Restriction Project Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
